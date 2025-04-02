7698f7c0a2af40fddfb570626e41ee3f72fb0ad9
rollingmountains <balakrishnapaudel@gmail.com>
2025-04-02 15:49:24 +0545

    create templates and base.html, use render_template()

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,24 @@
 from app import app
+from flask import render_template
+
+# mock data
+user = {'username': 'bond'}
+posts = [{
+    'title': 'Experience',
+    'author': 'John Doe',
+    'body': 'Good day in Kathmandu',
+    'date_published': '22, April 2024',
+},
+    {
+    'title': 'Movie time',
+    'author': 'Jane Smith',
+    'body': 'That was a wonderful movie.',
+    'date_published': '21, April 2024',
+}]
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return 'hello from dotenv'
+
+    return render_template('index.html', title="Home Page", user=user, posts=posts)
