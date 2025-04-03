0f6be0de76252286018497167e8258e514c82057
rollingmountains <balakrishnapaudel@gmail.com>
2025-04-03 15:08:31 +0545

    login form and submission enabled

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 # Application is hosted as a package
 from flask import Flask
+from config import Config  # import Config class from config file
+from dotenv import load_dotenv
 
+# set the instance of the app
 app = Flask(__name__)
 
-# registering the routes
+# load env variables
+load_dotenv()
+
+# set the app configs using env variables
+app.config.from_object(Config)
+
+# registering the routes in function to avoid moving to top upon save
 
 
 def register_routes():
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 # mock data
 user = {'username': 'bond'}
@@ -20,5 +21,14 @@ posts = [{
 @app.route("/")
 @app.route("/index")
 def index():
+    return render_template('index.html', title="Home Page", posts=posts)
 
-    return render_template('index.html', title="Home Page", user=user, posts=posts)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(
+            f'Login requested for {form.username.data}. Remember Me = {form.remember_me.data}')
+        return redirect(url_for('index'))
+    return render_template('login.html', title="Login Page", form=form)
