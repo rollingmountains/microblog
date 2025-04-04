60d1582614ff7965ef3b597b8db6e997f06180f8
rollingmountains <balakrishnapaudel@gmail.com>
2025-04-04 08:08:31 +0545

    user model creation

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from config import Config  # import Config class from config file
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 # set the instance of the app
 app = Flask(__name__)
@@ -11,12 +13,15 @@ load_dotenv()
 
 # set the app configs using env variables
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 # registering the routes in function to avoid moving to top upon save
 
 
 def register_routes():
-    from app import routes
+    from app import routes, models
 
 
 register_routes()
